2db5c38abeb77716db29129a8a6443518539bea1
Liudmila Ustsimenka <Liudmila.Ustsimenka@itechart-group.com>
2018-12-03 15:57:09 +0300

    chromeOptionsRunNoUI

--- conf.js
+++ conf.js
@@ -25,7 +25,7 @@ exports.config = {
     //seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.4.0.jar",
     directConnect: true,
 
-    specs: ['specs/sendEmailSpec.js'],
+    specs: ['specs/sendEmailSpec.js', 'specs/loginFailedSpec.js', 'specs/loginSuccessfullySpec.js'],
     baseUrl: 'https://mail.ru',
     framework: 'jasmine',
 
@@ -67,7 +67,8 @@ exports.config = {
                 '--disable-infobars',
                 '--disable-extensions',
                 'verbose',
-                'log-path=/tmp/chromedriver.log'
+                'log-path=/tmp/chromedriver.log',
+                '--headless'
             ],
             prefs: {
                 // disable chrome's annoying password manager
